1a6d26433069a77d3d19b52a34f2134590e21ce0
Sarika <sarikadaundkar6@gmail.com>
2026-01-27 15:17:38 +0530

    Login page objects and test cases

--- com.vittech.projectpractice/src/test/java/com/vittech/testecases/LoginTestCases.java
+++ com.vittech.projectpractice/src/test/java/com/vittech/testecases/LoginTestCases.java
@@ -1,5 +1,26 @@
 package com.vittech.testecases;
 
-public class LoginTestCases {
+import org.testng.annotations.Test;
+
+import com.microsoft.playwright.Page;
+import com.vittech.pages.LoginPage;
+import com.vittech.testbase.TestBase;
+
+public class LoginTestCases extends TestBase {
+	
+	
+	@Test()
+	public void login()
+	{
+		
+		
+		LoginPage lp = new LoginPage(page);
+		
+		lp.LoginPageUrl();
+		lp.LoginPageTitle();
+		lp.setPassword("sarikadaundkar6@gmail.com");
+		lp.setPassword("sarika");
+		lp.clickButton();
+	}
 
 }
