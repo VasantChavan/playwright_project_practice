7f450ed20f1884322b6eee3a899299d426b4dc87
Swati <swatimurkute1999@gmail.com>
2026-02-01 22:24:58 +0530

    Signup pages

---
+++ com.vittech.projectpractice/src/test/java/com/vittech/testecases/SignupTestCase.java
@@ -0,0 +1,44 @@
+
+package com.vittech.testecases;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import com.vittech.pages.Signup;
+import com.vittech.testbase.TestBase;
+
+public class SignupTestCase extends TestBase {
+	
+	
+	@Test(priority=1)
+public void verifySignupPageTitleTest() {
+	String actual_Title = sp.getSignPageTitle();
+	Assert.assertEquals(actual_Title, "Learn Automation Courses");
+}
+@Test(priority=2)
+public void verifyLoginPageURLTest() {
+	String actual_URL=sp.getSignPageURL();
+	Assert.assertEquals(actual_URL, "https://freelance-learn-automation.vercel.app/signup");
+	
+}
+	@Test()
+	public void Signup()
+	{
+				
+		Signup sp = new Signup(page);
+		
+		sp.getSignPageTitle();
+		sp.getSignPageURL();
+		sp.setUsername("Swati");
+		sp.setEmail("swatimurkute1999@gmail.com");
+		sp.setPassword("123456");
+		sp.setCheckbox();
+		sp.setCheckbox1();
+		sp.setRadio();
+		sp.setRadio1();
+		sp.setState();
+		sp.setHobbies();		
+		sp.clickButton();
+	}
+
+}
